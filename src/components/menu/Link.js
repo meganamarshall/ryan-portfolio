0b0dcdc4541966e15dcb26ea2d6db5f8181e310d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-09-17 14:09:35 -0700

    embedding bandcamp

--- src/components/menu/Link.js
+++ src/components/menu/Link.js
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 
 function Link({ link }) {
   return (
-    <li className="one-link">
-      <img className="albumCover" dangerouslySetInnerHTML={link.iFrame}></img>
-      <a href={link.url}>{link.title}</a>
-    </li>
+    <iframe src={link.src} width={link.width} height={link.height} seamless><a href={link.href}>{link.a}</a></iframe>
   );
 }
 
--- src/components/menu/Links.js
+++ src/components/menu/Links.js
@@ -5,36 +5,96 @@ import Link from './Link';
 const links = [
   {
     id: 1,
-    iFrm: '<iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=786701825/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="http://ambitionsound.bandcamp.com/album/atrophy-torque-fly">Atrophy Torque Fly by Ryan Albert Miller</a></iframe>',
-    title: 'Atrophy Torque Fly'
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=786701825/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '343px',
+    href: 'http://ambitionsound.bandcamp.com/album/atrophy-torque-fly',
+    a: 'Atrophy Torque Fly by Ryan Albert Miller'
   }, 
   {
+    id: 4,
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=3979042209/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '342px',
+    href: 'http://ryanamiller.bandcamp.com/album/hex-fortunes',
+    a: 'Hex Fortunes by Ryan A. Miller'
+  },
+  {
     id: 2,
-    iFrm: '<iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=2690566646/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="http://usco.bandcamp.com/album/tuskflower">TUSKFLOWER by U SCO</a></iframe>',
-    title: 'U Sco'
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=2690566646/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '342px',
+    href: 'http://usco.bandcamp.com/album/tuskflower',
+    a: 'TUSKFLOWER by U SCO'
+  },
+  {
+    id: 6,
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=2234186901/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '343px',
+    href: 'http://witheyesabstract.bandcamp.com/album/wea',
+    a: 'WEA by With Eyes Abstract'
+  },
+  {
+    id: 5, 
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=3324194137/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '343px',
+    href: 'http://partytentrecordings.bandcamp.com/album/singed-edges-burnt-ends',
+    a: 'singed edges / burnt ends by Noah Bernstein, Andrew Jones, Ricardo Lagomasino, Ryan Miller'
   },
   {
     id: 3,
-    title: 'With Eyes Abstract',
-    iFrm: '<iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=1364143066/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="http://witheyesabstract.bandcamp.com/album/fire-at-the-library">Fire at The Library by With Eyes Abstract</a></iframe>'
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=1364143066/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '343px',
+    href: 'http://witheyesabstract.bandcamp.com/album/fire-at-the-library',
+    a: 'Fire at The Library by With Eyes Abstract'
   },
   {
-    id: 4,
-    title: 'Ryan Albert Miller',
-    iFrm: '<iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=3979042209/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="http://ryanamiller.bandcamp.com/album/hex-fortunes">Hex Fortunes by Ryan A. Miller</a></iframe>'
+    id: 10,
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=962893025/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '342px',
+    href: 'http://newatlantisrecords.bandcamp.com/album/treffpunkt',
+    a: 'Treffpunkt by U Sco'
+  },
+  {
+    id: 7,
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=2034077445/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '342px',
+    href: 'http://witheyesabstract.bandcamp.com/album/return-of-the-heracleidae',
+    a: 'Return of The Heracleidae by With Eyes Abstract'
+  },
+  {
+    id: 11,
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=1917057191/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '342px',
+    href: 'http://newatlantisrecords.bandcamp.com/album/nest',
+    a: 'Nest by U Sco'
+  },
+  {
+    id: 8,
+    src: 'https://bandcamp.com/EmbeddedPlayer/album=1909102063/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/',
+    width: '200px',
+    height: '342px',
+    href: 'http://witheyesabstract.bandcamp.com/album/synaesthesiatic-ep',
+    a: 'Synaesthesiatic ep by With Eyes Abstract'
   }
 ];
 
 function Links({ section, visibleId, onClick }) {
   const linkList = links.map(link => (
-    <section className="linksList" key={link.id} >
+    <li key={link.id} >
       <Link link={link} />
-    </section>
+    </li>
   ));
   return (
     <>
       <h1 onClick={() => onClick(section.id)}>{section.title}</h1>
-      {visibleId === section.id && <section>{linkList}</section>}
+      {visibleId === section.id && <ul>{linkList}</ul>}
     </>
   );
 }
