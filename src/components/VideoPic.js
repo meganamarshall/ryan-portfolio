e96c4a088040ffd44bd9a9229e44a1cd26d32240
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-09-26 13:50:46 -0700

    widget and image fixes

--- src/components/VideoPic.js
+++ src/components/VideoPic.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import VideoPicStyled from '../styles/VideoPicStyled';
+import image from '../assets/IMG_2644.jpeg';
 
 export default function VideoPic() {
   return (
     <VideoPicStyled>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/0TBhq5NltKs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <img src="src/assets/IMG_2644.jpeg"></img>
+      <img src={image}></img>
     </VideoPicStyled>
   );
 }
--- src/components/menu/ContactInfo.js
+++ src/components/menu/ContactInfo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import logo from '../../assets/icons8-instagram-100.png';
 
 function ContactInfo({ section, visibleId, onClick }) {
   
   return (
     <>
-      {visibleId === section.id && <section className="contact" onClick={() => onClick(section.id)}><a href="mailto:ryanalbertmiller@gmail.com">{section.content}</a><a href="http://instagram.com/ryverb"><img src={section.icon}></img></a></section>}
+      {visibleId === section.id && <section className="contact" onClick={() => onClick(section.id)}><a href="mailto:ryanalbertmiller@gmail.com">{section.content}</a><a href="http://instagram.com/ryverb"><img src={logo}></img></a></section>}
     </>
   );
 }
