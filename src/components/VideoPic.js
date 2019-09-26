393b52085c35a803ddfb00bdfa62b9ace8674ed6
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-09-26 13:20:33 -0700

    fixing image pathing

--- src/components/VideoPic.js
+++ src/components/VideoPic.js
@@ -5,7 +5,7 @@ export default function VideoPic() {
   return (
     <VideoPicStyled>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/0TBhq5NltKs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <img src="/src/assets/IMG_2644.jpeg"></img>
+      <img src="/assets/IMG_2644.jpeg"></img>
     </VideoPicStyled>
   );
 }
