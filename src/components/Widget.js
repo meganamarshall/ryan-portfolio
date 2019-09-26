ab7344457b3c9b3f1358080ab86a7bfc8ac41093
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-09-26 14:00:02 -0700

    widget fix

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -4,7 +4,7 @@ import WidgetStyled from '../styles/WidgetStyled';
 export default function Widget() {
   return (
     <WidgetStyled>
-      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="https://cdn.lightwidget.com/widgets/a536fca959235ba9adc09df61c7df60d.html" scrolling="no" allowTransparency="true" className="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="https://cdn.lightwidget.com/widgets/a536fca959235ba9adc09df61c7df60d.html" scrolling="yes" className="lightwidget-widget"></iframe>
 
     </WidgetStyled>
   );
