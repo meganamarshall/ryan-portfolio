a069eb046ea2ec3c4bb7fb56bfaf9c7b3333b643
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-09-30 10:05:44 -0700

    removing animation to fix widget

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -49,21 +49,7 @@ const GlobalStyle = createGlobalStyle `
     border-collapse: collapse;
     border-spacing: 0;
   }
-  
-  * {
-    background-color: rgb(197, 168, 49);
-    animation: fadeInAnimation ease 1.5s;
-    animation-iteration-count: 1; 
-    animation-fill-mode: forwards; 
-  }
 
-  @keyframes fadeInAnimation { 
-  0% { 
-      opacity: 0; 
-  } 
-  100% { 
-      opacity: 1; 
-  } 
   `;
 
-  export default GlobalStyle;
+export default GlobalStyle;
